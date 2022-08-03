c97ea18d4354d6cea276d3fe174c83e662ef90dc
Corentin Deret <corentin.deret@payfit.com>
2022-08-03 11:46:45 +0200

    photos

--- content/photography/assets/js/live_update.js
+++ content/photography/assets/js/live_update.js
@@ -274,7 +274,7 @@ document.liveUpdateImageMaxSize = function( id, value ) {
 var storedGalleryAuthorURL = "";
 
 document.liveUpdate = function( path, newValue, cssId, property ) {
-    //window.alert(path + ' ' + newValue + ' ' + cssId);
+    // window.alert(path + ' ' + newValue + ' ' + cssId);
     var result = "failed",
         $el;
 
@@ -290,7 +290,7 @@ document.liveUpdate = function( path, newValue, cssId, property ) {
             $el = $("#galleryAuthor");
             if(newValue != ""){
                 if(storedGalleryAuthorURL != ""){
-                    $el.html('<a id="galleryAuthorURL" href="' + storedGalleryAuthorURL + '" target="_blank">' + "by " + newValue + '</a>');
+                    $el.html('<a id="galleryAuthorURL" href="' + storedGalleryAuthorURL + '" target="_blank">by ' + newValue + '</a>');
                 }
                 else {
                     $el.html("by " + newValue);
@@ -321,7 +321,12 @@ document.liveUpdate = function( path, newValue, cssId, property ) {
 
             $("#galleryAuthor").html(newAuthorContent);
 
-            break;     
+            break;
+
+        case "nonCSS.galleryExtra.value":
+            $el = $("#galleryExtra");
+            $el.html(newValue);
+            break;        
 
         // Color Palette
 
@@ -391,29 +396,7 @@ document.liveUpdate = function( path, newValue, cssId, property ) {
                 }
             );
             result = "invalidateAllContent";
-            break;
-
-        case "appearance.thumbnail.border-width":
-            
-            $el = $(".thumb-img");
-            $el.each(
-                function(index){
-                    this.style.setProperty('border-width', newValue, 'important' );
-                }
-            );
-            result = "invalidateAllContent";
-            break;
-
-        case "appearance.thumbnail.box-shadow":
-        
-            $el = $(".thumb-img");
-            $el.each(
-                function(index){
-                    this.style.setProperty('box-shadow', newValue, 'important' );
-                }
-            );
-            result = "invalidateAllContent";
-            break;    
+            break;              
 
         // The user changed the thumbnail size
         case "metadata.thumbnailSize.value":
@@ -421,8 +404,7 @@ document.liveUpdate = function( path, newValue, cssId, property ) {
             $el = $("#thumbnailContainer div.thumbnails");
             $el.removeClass("thumbnail-size-sm thumbnail-size-md thumbnail-size-lg").addClass("thumbnail-size-" + newValue);
             window.location.reload(true);
-            result = "invalidateAllContent";
-            break;   
+            break;
 
         // The user changed the header visibility
         case "nonCSS.showHeader":
@@ -444,6 +426,7 @@ document.liveUpdate = function( path, newValue, cssId, property ) {
                 __class = "is-fixed";
             }
             $el.removeClass("is-fixed").addClass(__class);
+            $("#thumbnailContainer").css("padding-top", $("header").outerHeight() + "px");
             break;
 
         // The user changed the photo corners setting
